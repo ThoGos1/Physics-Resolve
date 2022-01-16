abd8da38c36d7c10178465627cc23155e6dbb40b
ThoGos1 <tgosport@gmail.com>
2022-01-15 21:46:35 -0800

    added and made repo

---
+++ Physics Resolve/NoteView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteView.swift
+//  Physics Resolve
+//
+//  Created by Thomas Gosvig on 1/15/22.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text("suiiii")
+    }
+}
+
+struct NoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteView()
+    }
+}

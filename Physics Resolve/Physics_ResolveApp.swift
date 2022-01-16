e75da8415ee8ccabfe9cb1284814ae4eecbfdf01
ThoGos1 <tgosport@gmail.com>
2022-01-15 21:41:51 -0800

    Initial Commit

---
+++ Physics Resolve/Physics_ResolveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Physics_ResolveApp.swift
+//  Physics Resolve
+//
+//  Created by Thomas Gosvig on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Physics_ResolveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
